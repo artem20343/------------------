1368781c898e258b7d62d9d9ea656d0b06aeda9c
Artem Savich <dmiitr1evv@mail.ru>
2023-10-02 17:26:32 +0300

    Laboratornie raboti

--- Лаба 1 АИП/Савич Артем. Лаба 1/Задание 1.pas
+++
@@ -1,4 +0,0 @@
-﻿program HelloWorld;
-begin
-  writeln('Привет, мир!');
-end.
--- Лаба 2 ОАиП/Лаба 2 ОАиП/Задание 1.pas
+++
@@ -1,9 +0,0 @@
-var
-y,x,g: integer;
-begin
-writeln('Введите целое число');
-readln(x);
-if x<=2 then y:=x;
-if x>=3 then y:=2 else y:=-x+5;
-writeln( 'Ответ:',y);
-end.
