445643d3d6a079e30427cdfcfad279c7a2bef937
Artem Savich <145937608+artem20343@users.noreply.github.com>
2023-10-13 10:36:18 +0300

    Add files via upload

---
+++ ООП/Лаба 6 ООП/Задание4/main.py
@@ -0,0 +1,21 @@
+# Импортируем класс PotatoBed из модуля potato_bed
+from potato_bed import PotatoBed
+
+# Создаем объект класса PotatoBed с 5 грядками
+potato_bed = PotatoBed(5)
+
+# Выращиваем картошку на каждой грядке
+for _ in range(3):
+    potato_bed.grow_potatoes()
+    
+    # Выводим информацию о каждой картошке
+    for potato in potato_bed.potatoes:
+        print(f"Картошка {potato.number} сейчас {potato.get_stage()}")
+    
+    # Проверяем, все ли картошки созрели
+    if potato_bed.check_maturity():
+        print("Вся картошка созрела. Можно собирать!")
+    else:
+        print("Картошка ещё не созрела!")
+    
+    print()
